6bfeeef022bb819abb4fdafb4e79fcd0f13d83f2
markusms <33954500+markusms@users.noreply.github.com>
2019-12-28 15:09:21 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace MsecureToFsecure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //change path to where you have a keys.txt file with copy pasted msecure keys.csv
+            //change newPath to where you want the fsecureKeys.txt to be created 
+            //create a new csv file in excel and copy paste fsecureKeys.txt there
+            //import the new csv file to F-Secure KEY as Keepass 2 csv
+            string path = @"I:\Koodaus\CSharp\MsecureToFsecure\MsecureToFsecure\keys.txt";
+            string newPath = @"I:\Koodaus\CSharp\MsecureToFsecure\MsecureToFsecure\fsecureKeys.txt";
+
+            int counter = 0;
+            string line;
+            List<string> keyList = new List<string>();
+
+            StreamReader file = new StreamReader(path);
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] keys = line.Split(',');
+                int lastPos = keys.Length - 1;
+                string key = $"{keys[2]},{keys[5]},{keys[6]},{keys[4]},{keys[3]}";
+                keyList.Add(key);
+                counter++;
+            }
+            file.Close();
+
+            TextWriter tw = new StreamWriter(newPath, true);
+
+            tw.WriteLine("start"); //the first line has to have some text but it is not read as a password
+            foreach (var key in keyList)
+            {
+                tw.WriteLine(key);
+            }
+            tw.Close();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MSecureToFSecure
+ C# program that converts exported mSecure keys to a format that can be imported to F-Secure KEY
